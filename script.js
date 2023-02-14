56f2ed3e20c066262386dd42fe7e8755875a5507
khalid issa <khalidissa26@gmail.com>
2023-02-14 00:27:12 -0500

    fin game

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-var wins=0
-var loss=0
-var ties=0 
-var options =prompt['R', 'P','S' ]
+var wins=0;
+var loss=0;
+var ties=0;
+var options =['R', 'P', 'S' ];
 
 var playgame = function() {
-    var userchoice = window.prompt('Enter R,P or S:');
+    var userChoice = window.prompt('Enter R, P or S:');
 
-    if (!userchoice) {
-        return;
+    if (!userChoice) {
+        return; 
     }
-}
-var number=math.floor(math.random()*options.length)
-var computerchoice= options[number]
+
+
+var index = Math.floor(Math.random() * options.length)
+var computerchoice= options[index]
 window.alert('the pc chose' + computerchoice)
 
+if (userChoice==computerchoice){
+    ties++,
+    window.alert( 'its a tie!');
+}
+else if(
+       (userChoice==='R'&& computerchoice==='P')||
+       (userChoice==='P'&& computerchoice==='S')||
+       (userChoice==='S'&& computerchoice==='R')
+){
+       loss++;
+       window.alert('computer wins')
+} else{
+        wins++
+        window.alert('you win!')
+}
+window.alert(
+    'stats:\nwins: ' + wins + '\nlosses: ' +loss + '\nTies: ' +ties 
+)
+var playagain=window.confirm('play again?')
+if(playagain){
+    playgame()
+}
+}
+playgame()
